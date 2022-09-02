79bb45299cf9b91b4e3f09f2ccc68a5aa412c539
Anny Beatriz <anny.ribeiro@parnamirim.rn.gov.br>
2022-09-02 13:17:55 -0300

    'botao'

--- controller/static/js/login.js
+++
@@ -1,9 +0,0 @@
-function validateFields() {
-    let login =  document.getElementById("login").value;
-    if(!login) {
-        document.getElementById('try').disabled = true;
-    }
-    else {
-
-    }
-}
